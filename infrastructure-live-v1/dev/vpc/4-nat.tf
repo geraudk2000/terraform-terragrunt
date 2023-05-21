947de7c3d9cfc5b490d501b3bf21dea74ae021bd
Geraud <geraudkameni@gmail.com>
2023-05-21 19:57:41 +0200

    add routes and outputs

---
+++ infrastructure-live-v1/dev/vpc/4-nat.tf
@@ -0,0 +1,19 @@
+# Create an elastic IP for nat
+resource "aws_eip" "nat" {
+  vpc = true
+
+  tags = {
+    Name = "dev-nat"
+  }
+}
+
+# Nat should be place to public subnet
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_us_east1a.id
+
+  tags = {
+    Name = "dev-nat"
+  }
+  depends_on = [aws_internet_gateway.igw]
+}
