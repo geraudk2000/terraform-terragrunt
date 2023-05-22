a143e84dc638e8533ff1f8b3e363b7ec91f580b6
Geraud <geraudkameni@gmail.com>
2023-05-22 05:42:58 +0200

    correct route.tf

--- infrastructure-live-v1/staging/vpc/0-provider.tf
+++ infrastructure-live-v1/staging/vpc/0-provider.tf
@@ -1,13 +1,18 @@
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"    
+  region = "us-east-1"
+  alias = "dev"
+
+  access_key = var.dev_aws_key
+  secret_key = var.dev_aws_secret
+
 }
 
 terraform {
   required_version = ">=1.4"
 
   backend "local" {
-    path = "dev/vpc/terraform.tfstate"
+    path = "staging/vpc/terraform.tfstate"
   }
   required_providers {
     aws = {
