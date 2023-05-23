a7ba202f00965ae5e0b62391d787ab28eef9414f
Geraud <geraudkameni@gmail.com>
2023-05-23 05:18:29 +0200

    modified files

---
+++ infrastructure-modules/vpc/4-nat.tf
@@ -0,0 +1,19 @@
+# Create an elastic IP for nat
+resource "aws_eip" "this" {
+  vpc = true
+
+  tags = {
+    Name = "${var.env}-nat"
+  }
+}
+
+# Nat should be place to public subnet
+resource "aws_nat_gateway" "this" {
+  allocation_id = aws_eip.this.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = {
+    Name = "${var.env}-nat"
+  }
+  depends_on = [aws_internet_gateway.this]
+}
